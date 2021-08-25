e9cbec2e0609c3a6c453dbbe81d55993210eb34f
codebygabe <codebygabe@gmail.com>
2021-08-25 17:14:45 +0100

    Setup game structure

---
+++ HangMan/Program.cs
@@ -0,0 +1,38 @@
+﻿
+StartGame();
+PlayGame();
+EndGame();
+
+
+static void StartGame()
+{
+    Console.WriteLine("Starting the game...");
+    AskForUsersName();
+}
+
+static void AskForUsersName()
+{
+    Console.WriteLine("Asking user for name...");
+}
+
+static void PlayGame()
+{
+    Console.WriteLine("Playing the game...");
+    DisplayMaskedWord();
+    AskForLetter();
+}
+
+static void DisplayMaskedWord()
+{
+    Console.WriteLine("Displaying masked word...");
+}
+
+static void AskForLetter()
+{
+    Console.WriteLine("Asking for letter...");
+}
+
+static void EndGame()
+{
+    Console.WriteLine("Game over...");
+}
