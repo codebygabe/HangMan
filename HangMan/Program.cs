07bbb2a3df3106c80639536bcdb499b1f3cec81d
codebygabe <codebygabe@gmail.com>
2021-08-27 16:32:34 +0100

    Updated HangMan

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,37 +1,54 @@
 ﻿
-StartGame();
-PlayGame();
-EndGame();
+namespace HangMan;
 
-static void StartGame()
+class HangMan
 {
-    Console.WriteLine("Starting the game...");
-    AskForUsersName();
-}
+    static string name = string.Empty;
+    static int guesses;
 
-static void AskForUsersName()
-{
-    Console.WriteLine("Asking user for name...");
-}
+    static void Main(string[] args)
+    {
+        StartGame();
+        PlayGame();
+        EndGame();
+    }
 
-static void PlayGame()
-{
-    Console.WriteLine("Playing the game...");
-    DisplayMaskedWord();
-    AskForLetter();
-}
+    static void StartGame()
+    {
+        Console.WriteLine("Starting the game...");
+        AskForUsersName();
+    }
 
-static void DisplayMaskedWord()
-{
-    Console.WriteLine("Displaying masked word...");
-}
+    static void AskForUsersName()
+    {
+        Console.Write("Enter your name: ");
+        var input = Console.ReadLine() ?? "Player";
+        if (input.Length >= 2) name = input;
+        else AskForUsersName();
+    }
 
-static void AskForLetter()
-{
-    Console.WriteLine("Asking for letter...");
-}
+    static void PlayGame()
+    {
+        Console.WriteLine("Playing the game...");
+        DisplayMaskedWord();
+        AskForLetter();
+    }
 
-static void EndGame()
-{
-    Console.WriteLine("Game over...");
+    static void DisplayMaskedWord()
+    {
+        Console.WriteLine("Displaying masked word...");
+    }
+
+    static void AskForLetter()
+    {
+        Console.WriteLine("Asking for letter...");
+        guesses++;
+    }
+
+    static void EndGame()
+    {
+        Console.WriteLine("Game over...");
+        Console.WriteLine($"Thanks for playing, {name}");
+        Console.WriteLine($"You made {guesses} guesses");
+    }
 }
