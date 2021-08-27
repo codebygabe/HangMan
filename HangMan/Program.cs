f1a4a5de819fab5a45d574cf1e72d1517a2ccd79
codebygabe <codebygabe@gmail.com>
2021-08-27 17:14:02 +0100

    Worked on HangMan

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -3,10 +3,12 @@ namespace HangMan;
 
 class HangMan
 {
+    static readonly string correctWord = "hangman";
+    static char[]? letters;
     static string name = string.Empty;
-    static int guesses;
+    static int guesses = 0;
 
-    static void Main(string[] args)
+    static void Main()
     {
         StartGame();
         PlayGame();
@@ -15,34 +17,68 @@ class HangMan
 
     static void StartGame()
     {
-        Console.WriteLine("Starting the game...");
+        letters = new char[correctWord.Length];
+        for (int i = 0; i < correctWord.Length; i++) 
+            letters[i] = '-';
         AskForUsersName();
     }
 
     static void AskForUsersName()
     {
         Console.Write("Enter your name: ");
-        var input = Console.ReadLine() ?? "Player";
-        if (input.Length >= 2) name = input;
-        else AskForUsersName();
+        string input = Console.ReadLine() ?? "Player";
+        if (input.Length >= 2) 
+            name = input;
+        else 
+            AskForUsersName();
     }
 
     static void PlayGame()
     {
-        Console.WriteLine("Playing the game...");
-        DisplayMaskedWord();
-        AskForLetter();
+        while (true)
+        {
+            Console.Clear();
+            DisplayMaskedWord();
+            char guessedLetter = AskForLetter();
+            CheckLetter(guessedLetter);
+            if (correctWord == new string(letters))
+                break;
+        }
+
+        Console.Clear();
+    }
+
+    private static void CheckLetter(char guessedLetter)
+    {
+        for (int i = 0; i < correctWord.Length; i++)
+        {
+            if (guessedLetter == correctWord[i]) 
+                letters[i] = guessedLetter;
+        }
     }
 
     static void DisplayMaskedWord()
     {
-        Console.WriteLine("Displaying masked word...");
+        foreach (char c in letters) 
+            Console.Write(c);
+
+        Console.WriteLine();
     }
 
-    static void AskForLetter()
+    static char AskForLetter()
     {
-        Console.WriteLine("Asking for letter...");
+        string input;
+        while (true)
+        {
+            Console.Write("Enter a letter: ");
+            input = Console.ReadLine() ?? string.Empty;
+            if (input.Length == 1) 
+                break;
+        }
+
         guesses++;
+
+        return input[0];
     }
 
     static void EndGame()
